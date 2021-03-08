2b1f9fc3930465c2c3be750d8c803400464de05a
Jakub Meysner <hello@jakubmeysner.me>
2021-03-08 21:59:48 +0100

    Add pre play menus

--- src/save.cpp
+++ src/save.cpp
@@ -11,12 +11,24 @@ void saveState(State *state) {
 
     if (state->playedOnce) {
         saveFile << "playedOnce true\n";
+    } else {
+        saveFile << "playedOnce false\n";
     }
 
     if (state->difficultyLevel == DifficultyLevel::EASY) {
         saveFile << "difficultyLevel easy\n";
+    } else if (state->difficultyLevel == DifficultyLevel::NORMAL) {
+        saveFile << "difficultyLevel normal\n";
     } else if (state->difficultyLevel == DifficultyLevel::HARD) {
         saveFile << "difficultyLevel hard\n";
+    }
+
+    if (state->unlockedLevel == Level::OVERWORLD) {
+        saveFile << "unlockedLevel overworld\n";
+    } else if (state->unlockedLevel == Level::MINES) {
+        saveFile << "unlockedLevel mines\n";
+    } else if (state->unlockedLevel == Level::HELL) {
+        saveFile << "unlockedLevel hell\n";
     }
 
     saveFile.close();
@@ -52,6 +64,12 @@ void save(State *state) {
         state->difficultyLevel = DifficultyLevel::EASY;
     } else if (data["difficultyLevel"] == "hard") {
         state->difficultyLevel = DifficultyLevel::HARD;
+    }
+
+    if (data["unlockedLevel"] == "mines") {
+        state->unlockedLevel = Level::MINES;
+    } else if (data["unlockedLevel"] == "hell") {
+        state->unlockedLevel = Level::HELL;
     }
 
     state->initialLoadPerformed = true;
